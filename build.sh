c89b5fb066b1b7123e43615c2283c2275b00c3fb
Nourl <nourlcn@gmail.com>
2012-04-10 14:58:23 +0800

    modify build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export MAVEN_OPTS=-Xmx1024m
 #mvn clean package -Pdist -Dtar -DskipTests
 #mvn clean package -Pdist -Dtar -DskipTests
 
-mvn package -Pdist -Dtar -DskipTests
+mvn clean package -Pdist -Dtar -DskipTests
