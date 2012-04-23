550cb9402aec901d841e7983c79440317f16cc35
Nourl <nourlcn@gmail.com>
2012-04-23 15:54:54 +0800

    modify auto_hadoop.sh

--- auto_hadoop.sh
+++ auto_hadoop.sh
@@ -15,12 +15,15 @@ cat $SRC/debug >> ./conf/yarn-env.sh
 cp $DST/share/hadoop/mapreduce/*example*.jar $DST/
 cp $SRC/input $DST/
 
-echo "pi"
-echo "----------"
+
+#scp -r -P1314 ./* hadoop@115.25.139.50:/home/hadoop/yarn/
+
+#echo "pi"
+#echo "----------"
 echo "bin/hadoop jar hadoop-mapreduce-examples-0.23.3-SNAPSHOT.jar pi 1 1"
 
-echo "wordcount"
-echo "--------------"
+#echo "wordcount"
+#echo "--------------"
 echo "bin/hadoop jar hadoop-mapreduce-examples-0.23.3-SNAPSHOT.jar wordcount file:///home/nourl/github/hadoop-common/hadoop-dist/target/hadoop-0.23.3-SNAPSHOT/input file:///home/nourl/github/hadoop-common/hadoop-dist/target/hadoop-0.23.3-SNAPSHOT/output"
 
 #source export.sh
---
+++ conf/yarn-env.sh
@@ -0,0 +1,7 @@
+
+
+
+
+YARN_RESOURCEMANAGER_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=5000"
+YARN_NODEMANAGER_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=5001"
+YARN_JOB_HISTORYSERVER_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=5002"
